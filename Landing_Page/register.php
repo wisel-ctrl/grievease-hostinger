a21fc313cff186d0bcc6633af17f4baa15b714d8
wisel-ctrl <186314040+wisel-ctrl@users.noreply.github.com>
2025-04-11 09:44:22 +0800

    jsa

--- Landing_Page/register.php
+++ Landing_Page/register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Funeral Service Registration</title>
-    <?php include '../faviconLogo.html'; ?>
+    <link rel="icon" type="image/png" href="Landing_images/logo.png">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&display=swap" rel="stylesheet">
