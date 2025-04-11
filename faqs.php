80ef4f85bb8a207f5a0959389def9db5e1a23397
wisel-ctrl <186314040+wisel-ctrl@users.noreply.github.com>
2025-04-11 09:34:46 +0800

    faqs, emmorial, services

--- faqs.php
+++ faqs.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GrievEase - Frequently Asked Questions</title>
+    <?php include 'faviconLogo.html'; ?>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&family=Alex+Brush&family=Inter:wght@300;400;500;600&family=Cinzel:wght@400;500;600;700&family=Hedvig+Letters+Serif:wght@400;500&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
