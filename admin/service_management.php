7aa73b4d699ee3cb020d7a3b26fe8435e6909c24
Riyelle Sanchez <rylsnchz@gmail.com>
2025-04-17 22:57:55 +0800

    x

--- admin/service_management.php
+++ admin/service_management.php
@@ -860,8 +860,8 @@ document.addEventListener('DOMContentLoaded', () => {
     
     <!-- Modal Footer --> 
     <div class="px-6 py-4 flex justify-end gap-4 border-t border-gray-200 sticky bottom-0 bg-white">
-      <button class="px-5 py-2 bg-white border border-sidebar-accent text-gray-800 rounded-lg font-medium hover:bg-gray-100 transition-all duration-200 flex items-center" onclick="closeAddServiceModal()">
-        <i class="fas fa-times bg-white mr-2"></i>
+      <button class="px-5 py-2 bg-white border border-sidebar-accent text-white rounded-lg font-medium hover:bg-gray-100 transition-all duration-200 flex items-center" onclick="closeAddServiceModal()">
+        <i class="fas fa-times mr-2"></i>
         Cancel
       </button>
       <button class="px-6 py-2 bg-gradient-to-r from-sidebar-accent to-darkgold text-white rounded-lg font-medium shadow-lg hover:shadow-xl transition-all duration-300 flex items-center justify-center" onclick="addService()">
