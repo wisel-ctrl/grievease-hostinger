b3b68905714156d5cde4c6e2782443f871717804
Riyelle Sanchez <rylsnchz@gmail.com>
2025-04-17 23:16:23 +0800

    hahahha

--- customer/index.php
+++ customer/index.php
@@ -85,6 +85,7 @@ header("Pragma: no-cache");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GrievEase - Customer Dashboard</title>
     <?php include 'faviconLogo.php'; ?>
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="tailwind.js"></script>
     <style>
