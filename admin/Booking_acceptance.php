f31e6a8baf9e8596a7e05569eaa88dcae50cec47
law-study <lawrencerebong09@gmail.com>
2025-04-21 17:42:42 +0800

    Update Booking_acceptance.php

--- admin/Booking_acceptance.php
+++ admin/Booking_acceptance.php
@@ -87,8 +87,7 @@ $offset = ($current_page - 1) * $bookings_per_page;
     <title>Bookings - GrievEase</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/js/all.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert2/11.4.8/sweetalert2.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert2/11.4.8/sweetalert2.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
 </head>
