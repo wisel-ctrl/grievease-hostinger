e06feaf2a5f1e5141f9362079cf7c34bcdbc47b7
Riyelle Sanchez <rylsnchz@gmail.com>
2025-04-22 21:53:02 +0800

    langya

--- admin/addEmployee/fetch_employee_accounts.php
+++ admin/addEmployee/fetch_employee_accounts.php
@@ -92,7 +92,7 @@ if ($result->num_rows > 0) {
                   <i class="fas fa-edit"></i>
                 </button>
                 <button class="p-1.5 bg-red-100 text-red-600 rounded hover:bg-red-200 transition-all" onclick="deleteEmployeeAccount(' . $row['id'] . ')">
-                   <i class="fas fa-archive text-sidebar-accent"></i>
+                   <i class="fas fa-archive text-red"></i>
                 </button>
               </div>
             </td>
