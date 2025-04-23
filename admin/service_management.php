efcc62a1308da45aed228220c3966b9cc9f8b8c7
Riyelle Sanchez <rylsnchz@gmail.com>
2025-04-23 21:22:12 +0800

    Update service_management.php

--- admin/service_management.php
+++ admin/service_management.php
@@ -447,7 +447,7 @@ if ($branchResult->num_rows > 0) {
                                 </td>
                                 <td class="px-4 py-3.5 text-sm">
                                     <div class="flex space-x-2">
-                                        <button class="p-2 bg-blue-100 text-blue-600 rounded-lg hover:bg-blue-200 transition-all tooltip" title="Edit Service" onclick="openEditServiceModal('<?php echo $row["service_id"]; ?>', '<?php echo $branchId; ?>')">
+                                        <button class="p-2 bg-yellow-100 text-yellow-600 rounded-lg hover:bg-yellow-200 transition-all tooltip" title="Edit Service" onclick="openEditServiceModal('<?php echo $row["service_id"]; ?>', '<?php echo $branchId; ?>')">
                                             <i class="fas fa-edit"></i>
                                         </button>
                                         <button class="p-2 bg-red-100 text-red-600 rounded-lg hover:bg-red-200 transition-all tooltip" title="Delete Service" onclick="deleteService('<?php echo $row["service_id"]; ?>', '<?php echo $branchId; ?>')">
