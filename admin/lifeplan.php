b9a0399731e9369752ebe23f91a51c21b155067e
Riyelle Sanchez <rylsnchz@gmail.com>
2025-04-23 23:20:09 +0800

    Update lifeplan.php

--- admin/lifeplan.php
+++ admin/lifeplan.php
@@ -232,7 +232,6 @@ if ($conn) {
 
         <!-- Status Dropdown Acting as Filter -->
         <select id="statusFilter" class="px-3 py-2 border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-sidebar-accent"> <i class="fas fa-filter text-sidebar-accent"></i> Filter
-          <option value=""> Filter</option>
           <option value="paid">Paid</option>
           <option value="ongoing">Ongoing</option>
           <option value="overdue">Overdue</option>
