db6bea81471c7c24b8413fe9f5b88c3f2e8cd6d8
law-study <lawrencerebong09@gmail.com>
2025-04-23 14:27:28 +0800

    Update lifeplan.php

--- admin/lifeplan.php
+++ admin/lifeplan.php
@@ -389,7 +389,7 @@ header("Pragma: no-cache");
                                           title="View Receipt" 
                                           data-id="' . $row['lifeplan_id'] . '"
                                           data-name="' . htmlspecialchars($row['benefeciary_fullname']) . '"
-                                          data-monthly="' . $row['custom_price'] . '"
+                                          data-custom-price="' . $row['custom_price'] . '"
                                           data-duration="' . $row['payment_duration'] . '"
                                           data-total="' . number_format($row['amount_paid']) . '"
                                           data-balance="' . number_format($row['balance']) . '">
