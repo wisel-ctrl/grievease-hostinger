f767cacd76458f0b8e78df7873328809998cb5e3
Riyelle Sanchez <rylsnchz@gmail.com>
2025-04-25 11:05:57 +0800

    Update account_management.php

--- admin/account_management.php
+++ admin/account_management.php
@@ -270,7 +270,6 @@ $customersResult = mysqli_query($conn, $customersQuery);
                         </div>
                     </div>
                 </div>
-            </div>
 
             <!-- Archive Icon Button -->
           <button class="w-10 h-10 flex items-center justify-center text-sidebar-accent" onclick="showArchivedItems(<?php echo $branchId; ?>)">
