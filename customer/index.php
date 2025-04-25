d5635a8125d66d029045893421e1454e4a4e59da
Riyelle Sanchez <rylsnchz@gmail.com>
2025-04-25 12:26:36 +0800

    s

--- customer/index.php
+++ customer/index.php
@@ -573,7 +573,7 @@ header("Pragma: no-cache");
             <div class="package-card min-w-full md:min-w-[33.333%] px-2 sm:px-4">
                 <div class="bg-white rounded-[20px] shadow-lg overflow-hidden relative group hover:shadow-xl transition-all duration-300 flex flex-col h-full" data-price="300000" data-service="traditional" data-name="Eternal Remembrance" data-image="../image/300.jpg">
                     <div class="h-10 sm:h-12 bg-navy flex items-center justify-center">
-                        <h4 class="text-white font-hedvig text-lg sm:text-xl">Eternal Remembrance</h4>
+                        <h4 class="text-white font-hedvig text-lg sm:text-xl" style="text-color: white">Eternal Remembrance</h4>
                         <div class="absolute top-0 right-0 w-12 h-12 sm:w-16 sm:h-16 flex items-center justify-center">
                             <div class="w-12 h-12 sm:w-16 sm:h-16 bg-yellow-600/90 rotate-45 transform origin-bottom-left"></div>
                         </div>
