9da6a13bf98280ee0e7d8a00f0db14c3106c2cde
Riyelle Sanchez <rylsnchz@gmail.com>
2025-04-26 12:47:56 +0800

    Update notification.php

--- customer/notification.php
+++ customer/notification.php
@@ -572,10 +572,6 @@ $current_page_bookings = array_slice($filtered_bookings, ($page - 1) * $items_pe
                     
                     <div class="bg-white border-l-4 <?php echo $status_color; ?> rounded-xl shadow-md overflow-hidden notification-animate hover:shadow-lg transition-all duration-300">
                         <div class="flex flex-col md:flex-row">
-                            <!-- Notification Icon -->
-                            <div class="<?php echo $status_bg; ?> py-4 px-4 md:w-16 flex items-center justify-center">
-                                <i class="<?php echo $status_icon; ?> <?php echo $status_text_color; ?> text-xl"></i>
-                            </div>
                             
                             <!-- Notification Content -->
                             <div class="flex-1 p-3">
