519bae215a0eb70f622ca619a4b80b23cf20d2a8
Riyelle Sanchez <rylsnchz@gmail.com>
2025-04-27 21:02:27 +0800

    Update profile.php

--- customer/profile.php
+++ customer/profile.php
@@ -1416,6 +1416,23 @@ header("Pragma: no-cache");
                 </div>
             </div>
         </div>
+
+        <?php if ($id_status === 'valid'): ?>
+<div class="bg-green-50 border-l-4 border-green-500 p-4 mb-6 rounded-lg">
+    <div class="flex items-center">
+        <div class="flex-shrink-0">
+            <svg class="h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="ml-3">
+            <p class="text-sm text-green-700">
+                Your ID has been successfully verified and approved.
+            </p>
+        </div>
+    </div>
+</div>
+<?php endif; ?>
         
         <!-- Example of a Correct ID Upload - Stack on mobile -->
         <div class="sm:block">
