769002038c8a7b9e779d34eea2d1fb0e275079b4
Riyelle Sanchez <rylsnchz@gmail.com>
2025-04-30 20:42:04 +0800

    Update profile.php

--- customer/profile.php
+++ customer/profile.php
@@ -582,9 +582,6 @@ header("Pragma: no-cache");
                         <div class="bg-gray-50 p-3 sm:p-4 rounded-lg">
                             <label class="block text-xs font-medium text-gray-500 mb-1">Date of Birth</label>
                             <p class="text-navy font-medium flex items-center text-sm sm:text-base">
-                                <svg class="w-3 h-3 sm:w-4 sm:h-4 mr-1 sm:mr-2 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path>
-                                </svg>
                                 <?php echo date('F d, Y', strtotime($birthdate)); ?>
                             </p>
                         </div>
