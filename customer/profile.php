4506ae7eebeaad3bcff6395b3e7dd13a0d08af5a
law-study <lawrencerebong09@gmail.com>
2025-05-06 18:43:34 +0800

    3

--- customer/profile.php
+++ customer/profile.php
@@ -1601,6 +1601,7 @@ function getSamplePaymentHistory(packageType) {
           
 </div>
 </div>
+</div>
     <!-- Footer -->
     <footer class="bg-black font-playfair text-white py-12">
         <div class="container mx-auto px-6">
