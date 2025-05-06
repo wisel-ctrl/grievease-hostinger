7950c583aafb4b8a1b611efa640b67a5d9dda981
Riyelle Sanchez <rylsnchz@gmail.com>
2025-05-06 20:05:39 +0800

    Update about.php

--- about.php
+++ about.php
@@ -347,7 +347,7 @@
 
 <!-- Image replacement -->
 <div class="max-w-5xl mx-auto px-4">
-    <img src="image/orgchart.png" alt="Organization Chart" class="w-full h-auto rounded-lg shadow-md">
+    <img src="image/orgchart.jpg" alt="Organization Chart" class="w-full h-auto rounded-lg shadow-md">
 </div>
 
 <div>
