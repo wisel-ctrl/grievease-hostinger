2b08958bf3ba1b139be161832b76c03389790ddb
law-study <lawrencerebong09@gmail.com>
2025-05-27 11:24:55 +0800

    Update update_custom_history_sales.php

--- employee/historyAPI/update_custom_history_sales.php
+++ employee/historyAPI/update_custom_history_sales.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-require_once '../db_connect.php';
+require_once '../../db_connect.php';
 
 // Function to handle file upload
 function handleFileUpload($file, $customsales_id, $conn) {
