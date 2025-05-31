172920024c9da574d9bccaf1a6869b7400bffd1b
law-study <lawrencerebong09@gmail.com>
2025-05-31 15:18:56 +0800

    Update history.php

--- admin/history.php
+++ admin/history.php
@@ -1280,6 +1280,8 @@ $offsetOutstanding = ($outstandingPage - 1) * $recordsPerPage;
     </div>
   </div>
 
+            </div>
+
   <!-- Modal for Editing Service -->
   <div class="fixed inset-0 z-50 flex items-center justify-center hidden" id="editServiceModal">
     <!-- Modal Backdrop -->
