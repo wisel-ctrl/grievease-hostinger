ae8d39655cf5226aebabb3f70d19313c3c21c30a
Riyelle Sanchez <rylsnchz@gmail.com>
2025-07-04 22:12:13 +0800

    Update profile.php

--- customer/profile.php
+++ customer/profile.php
@@ -801,7 +801,7 @@ input[name*="LastName"] {
                                         $statusText = 'APPROVED';
                                         $statusClass = 'bg-green-100 text-green-800 border border-green-200';
                                         $iconColor = 'text-green-500';
-                                        $icon = '<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 10-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>';
+                                        $icon = '<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>';
                                         break;
                                     case 'denied':
                                         $statusText = 'DECLINED';
