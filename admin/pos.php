8829c1186ceb4ea48eddc3238cf02f7faaa7761e
law-study <lawrencerebong09@gmail.com>
2025-10-05 16:30:05 +0800

    Update pos.php

--- admin/pos.php
+++ admin/pos.php
@@ -132,7 +132,7 @@ $userId = $_SESSION['user_id'];
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>GrievEase - Point-Of-Sale</title>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/js/all.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/js/all.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
   <script src="https://cdn.tailwindcss.com"></script>
   <style>
