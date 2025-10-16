c7ca40fc5657144f732136e24704c32d827832a6
law-study <lawrencerebong09@gmail.com>
2025-10-16 18:21:19 +0800

    Update employee_management.php

--- admin/employee_management.php
+++ admin/employee_management.php
@@ -106,6 +106,10 @@ $totalPages = ceil($totalEmployees / $perPage);
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/js/all.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+
+<!-- Flatpickr JS -->
+<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     <style>
       .modal-scroll-container {
     scrollbar-width: thin;
