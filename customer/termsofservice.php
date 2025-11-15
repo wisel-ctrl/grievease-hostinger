fab1a5bdcb84d82fff9c22a9a6728a4773c55f8e
Riyelle Sanchez <rylsnchz@gmail.com>
2025-11-15 13:43:08 +0800

    link

--- customer/termsofservice.php
+++ customer/termsofservice.php
@@ -7,7 +7,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700&family=Inter:wght@300;400;500;600&family=Cinzel:wght@400;500;600;700&family=Alex+Brush&family=Hedvig+Letters+Serif:wght@400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <script src="tailwind.js"></script>
+    <script src="../tailwind.js"></script>
 </head>
 <body class="bg-cream text-dark font-playfair">
     
