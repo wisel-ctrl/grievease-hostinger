e735eaeef0133d43c55ab3e9ed479c40d8afd131
Riyelle Sanchez <rylsnchz@gmail.com>
2025-11-15 13:26:46 +0800

    removed

--- termsofservice.php
+++ termsofservice.php
@@ -10,17 +10,7 @@
     <script src="tailwind.js"></script>
 </head>
 <body class="bg-cream text-dark font-playfair">
-    <!-- Breadcrumb Navigation -->
-    <div class="bg-white border-b border-gray-200 fixed top-[var(--navbar-height)] left-0 right-0 z-40">
-        <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-3">
-            <nav class="flex items-center text-sm" aria-label="Breadcrumb">
-                <ol id="dynamic-breadcrumb" class="flex items-center space-x-2">
-                    <!-- Breadcrumb will be populated by JavaScript -->
-                </ol>
-            </nav>
-        </div>
-    </div>
-    <script src="breadcrumb-navigation.js"></script>
+    
 
     <!-- Header Banner -->
     <div class="bg-black text-white py-16 px-6" style="margin-top: 48px;">
