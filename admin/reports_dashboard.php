051b1bebf8741f04a22efce4e984e39f6bb02842
law-study <lawrencerebong09@gmail.com>
2025-11-17 21:44:47 +0800

    Update reports_dashboard.php

--- admin/reports_dashboard.php
+++ admin/reports_dashboard.php
@@ -1153,7 +1153,7 @@ var options = {
   dataLabels: {
     enabled: true,
     style: {
-      colors: ['#6B7280'],
+      colors: ['#9CA3AF'],
       fontSize: '12px',
       fontWeight: 600,
       fontFamily: 'Inter, Helvetica, sans-serif',
